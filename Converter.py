5e8462c5ceff0901d0ddc89bd985cb81953d6d35
Dmitrii Zotov <85106362+dmzotov-ru@users.noreply.github.com>
2022-01-18 20:53:37 +0300

    Create Converter.py

---
+++ Converter.py
@@ -0,0 +1,28 @@
+class Converter:
+    @staticmethod
+    def kg_to_pound(kg):
+        return kg * 2.2046
+
+    
+    @staticmethod
+    def kg_to_oz(kg):
+        return kg * 35.274
+
+
+    @staticmethod
+    def oz_to_kg(oz):
+        return oz * 0.0283
+
+
+    @staticmethod
+    def pound_to_kg(pound):
+        return pound * 0.4535
+
+    @staticmethod
+    def oz_to_pound(oz):
+        return oz * 0.0625
+
+    
+
+    
+    
